95d1a73ccb8379d7c587f1311ac559c329acc79b
孙剑桥 <413326498@qq.com>
2022-07-26 14:01:46 +0800

    第三章，Spring data jpa 3.3

---
+++ README.md
@@ -0,0 +1,7 @@
+###Spring 实战(第 5 版)
+[Spring 实战(第 5 版)][https://potoyang.gitbook.io/spring-in-action-v5/]
+
+###Spring Data 解析 repository 方法特征来确定如何运行查询语句
+![img.png](resource/img.png)
+
+[https://potoyang.gitbook.io/spring-in-action-v5/]: https://potoyang.gitbook.io/spring-in-action-v5/
--- src/main/java/com/example/springboottest/dao/OrderRepository.java
+++ src/main/java/com/example/springboottest/dao/OrderRepository.java
@@ -2,6 +2,7 @@ package com.example.springboottest.dao;
 
 import com.example.springboottest.data.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
 import java.util.List;
@@ -11,4 +12,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByZip(String zip);
     //需要查询在给定日期范围内投递给指定邮政编码的所有订单
     List<Order> readOrdersByZipAndPlacedAtBetween(String zip, Date startDate, Date endDate);
+    //可以随意将方法命名为任何想要的名称，并使用 @Query 对其进行注解，以显式地指定调用方法时要执行的查询
+    @Query("Order o where o.deliveryCity='Seattle'")
+    List<Order> readOrdersDeliveredInSeattle();
 }
